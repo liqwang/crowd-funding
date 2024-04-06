588b0af78ed660db858b63c909756339ac843375
QuanQuan-CHO <12011619@mail.sustech.edu.cn>
2024-04-06 11:49:57 +0800

    chore(web-lib): add `definition.ts`

---
+++ web/app/lib/definition.ts
@@ -0,0 +1,15 @@
+export type Project = {
+    id: number
+    name: string
+    address?: string
+    ens?: string
+    description: string
+    targetFund: string
+    currentFund: string
+}
+
+export type User = {
+    address: string
+    ens: string
+    projects: Project[]
+}
