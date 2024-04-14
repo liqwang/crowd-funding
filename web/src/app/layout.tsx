590a10728f781b1eb6a31f163ec05f7799344ab4
QuanQuan-CHO <12011619@mail.sustech.edu.cn>
2024-04-15 00:02:20 +0800

    refactor(web-ui): simplify Navbar by using Iconify

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -22,20 +22,7 @@ export default function RootLayout({
         <div className="navbar bg-base-100 border-b-2 sticky top-0 z-50">
           <div className="navbar-start">
             <a className="btn btn-ghost text-lg">
-              {/* Projects icon */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block w-7 h-7 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
-                />
-              </svg>
+              <i className="icon-[tabler--archive] w-7 h-7"/>
               Projects
             </a>
             <a className="btn btn-ghost text-lg">
@@ -46,37 +33,12 @@ export default function RootLayout({
           <div className="navbar-center">
             <label className="input input-bordered flex items-center gap-2">
               <input type="text" className="grow" placeholder="Search" />
-              {/* magnifying glass icon */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="w-5 h-5 opacity-70"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <i className="icon-[tabler--search] w-6 h-6" />
             </label>
           </div>
           <div className="navbar-end">
             <a className="btn btn-ghost text-lg">
-              {/* Home icon */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block w-7 h-7 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                />
-              </svg>
+              <i className="icon-[charm--person] w-7 h-7"/>
               Home
             </a>
           </div>
