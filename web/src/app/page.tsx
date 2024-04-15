9c2f18129f67906579981771e2a2b831cd559971
QuanQuan-CHO <12011619@mail.sustech.edu.cn>
2024-04-15 20:34:58 +0800

    feat(web-ui): add main page cards

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,35 @@
-export default function Home() {
-  return <div />
+//card for a project
+function Card () {
+  return (
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+      <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+      <div className="card-body">
+        <h2 className="card-title">Shoes</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Detail</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default function Projects() {
+  return (
+    // https://tailwindcss.com/docs/margin
+    <div className="flex justify-center my-8">
+      {/* https://tailwindcss.com/docs/grid-template-columns#breakpoints-and-media-queries */}
+      <div className="grid gap-4 xl:grid-cols-3 md:grid-cols-2">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
+    </div>
+  )
 }
