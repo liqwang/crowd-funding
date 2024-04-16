3531da9e3b48122a90eda12efb24a8eb70baf871
QuanQuan-CHO <12011619@mail.sustech.edu.cn>
2024-04-16 20:18:47 +0800

    feat(web-ui): add `/new` page

--- web/src/app/new/page.tsx
+++ web/src/app/new/page.tsx
@@ -5,5 +5,22 @@ export const metadata: Metadata = {
 }
 
 export default function NewProject() {
-  return <div/>
+  return (
+    // https://tailwindcss.com/docs/flex-direction#column
+    // https://tailwindcss.com/docs/width#percentage-widths
+    <div className="flex flex-col w-1/3 text-sm">
+      <label className="font-semibold ml-1 mb-1">Title</label>
+      {/* https://daisyui.com/components/input */}
+      <input type="text" className="input input-bordered input-sm"/>
+
+      <label className="font-semibold ml-1 mb-1 mt-4">ETH address / ENS domain</label>
+      <input type="text" className="input input-bordered input-sm w-2/3"/>
+
+      <label className="font-semibold ml-1 mb-1 mt-4">Target Fund (ETH)</label>
+      <input type="text" className="input input-bordered input-sm w-1/2"/>
+
+      <label className="font-semibold ml-1 mb-1 mt-4">Description</label>
+      <textarea className="textarea textarea-bordered h-64"/>
+    </div>
+  )
 }
