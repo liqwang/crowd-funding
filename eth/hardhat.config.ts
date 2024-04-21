428f9c9538d44fbc7ea257745a0d8bb89182be4e
QuanQuan-CHO <12011619@mail.sustech.edu.cn>
2024-04-21 20:53:38 +0800

    feat(eth): deploy contract to Sepolia testnet https://ethereum.stackexchange.com/questions/145719/understanding-how-metamask-queries-the-balance-of-an-eth-address-from-a-rpc-node

--- eth/hardhat.config.ts
+++ eth/hardhat.config.ts
@@ -1,8 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config"
-import "@nomicfoundation/hardhat-toolbox"
+import 'dotenv/config'
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      // https://ethereum.stackexchange.com/questions/145719/understanding-how-metamask-queries-the-balance-of-an-eth-address-from-a-rpc-node
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`${process.env.ACCOUNT1_PRIVATE_KEY}`],
+      chainId: 11155111
+    }
+  },
   paths: { // https://hardhat.org/hardhat-runner/docs/config#path-configuration
     sources: "./contract"
   }
