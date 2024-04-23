da3316c075e26e498c7fe567a8738e8b385acf95
QuanQuan-CHO <12011619@mail.sustech.edu.cn>
2024-04-23 20:12:17 +0800

    refactor(eth): migrate from `dotenv` to hardhat `vars` https://hardhat.org/hardhat-runner/docs/guides/configuration-variables

--- eth/hardhat.config.ts
+++ eth/hardhat.config.ts
@@ -1,5 +1,4 @@
-import 'dotenv/config'
-import { HardhatUserConfig } from 'hardhat/config'
+import { HardhatUserConfig, vars } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 
 const config: HardhatUserConfig = {
@@ -7,13 +6,13 @@ const config: HardhatUserConfig = {
   networks: {
     sepolia: {
       // https://ethereum.stackexchange.com/questions/145719/understanding-how-metamask-queries-the-balance-of-an-eth-address-from-a-rpc-node
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [`${process.env.ACCOUNT1_PRIVATE_KEY}`],
+      url: `https://sepolia.infura.io/v3/${vars.get('INFURA_PROJECT_ID')}`,
+      accounts: [vars.get('ACCOUNT1_PRIVATE_KEY')],
       chainId: 11155111
     }
   },
   etherscan: { // https://hardhat.org/ignition/docs/guides/verify
-    apiKey: process.env.ETHERSCAN_API_KEY
+    apiKey: vars.get('ETHERSCAN_API_KEY')
   },
   paths: { // https://hardhat.org/hardhat-runner/docs/config#path-configuration
     sources: "./contract"
