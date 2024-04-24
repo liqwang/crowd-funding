372fda41b7a5949154f488f8f0d31363ffa46189
QuanQuan-CHO <12011619@mail.sustech.edu.cn>
2024-04-25 03:08:47 +0800

    feat(web): implement upload image

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { // https://nextjs.org/docs/messages/next-image-unconfigured-host
+        protocol: 'https',
+        hostname: 'img2.imgtp.com',
+        port: '', // shouldn't be set to '443', which causes error in http://localhost:3000
+        pathname: '/**'
+      }
+    ]
+  }
+}
 
 export default nextConfig
--- web/src/app/new/page.tsx
+++ web/src/app/new/page.tsx
@@ -1,26 +1,68 @@
-import type { Metadata } from 'next'
+'use client'
 
-export const metadata: Metadata = {
-  title: 'New Project'
-}
+import Head from 'next/head'
+import Image from 'next/image'
+import { useState } from 'react'
 
 export default function NewProject() {
+  const [imageUrl, setImageUrl] = useState('')
+  const [uploading, setUploading] = useState(false)
   return (
-    // https://tailwindcss.com/docs/flex-direction#column
-    // https://tailwindcss.com/docs/width#percentage-widths
-    <div className="flex flex-col w-1/3 text-sm">
-      <label className="font-semibold ml-1 mb-1">Title</label>
-      {/* https://daisyui.com/components/input */}
-      <input type="text" className="input input-bordered input-sm"/>
+    <> {/* https://react.dev/reference/react/Fragment */}
+      <Head>
+        <title>My New Title</title>
+      </Head>
+      {/* https://tailwindcss.com/docs/flex-direction#column */}
+      {/* https://tailwindcss.com/docs/width#percentage-widths */}
+      <div className="flex flex-col w-1/3 text-sm">
+        <label className="font-semibold ml-1 mb-1">Title</label>
+        {/* https://daisyui.com/components/input */}
+        <input type="text" className="input input-bordered input-sm"/>
 
-      <label className="font-semibold ml-1 mb-1 mt-4">ETH address / ENS domain</label>
-      <input type="text" className="input input-bordered input-sm w-2/3"/>
+        <label className="font-semibold ml-1 mb-1 mt-4">ETH address / ENS domain</label>
+        <input type="text" className="input input-bordered input-sm w-2/3"/>
 
-      <label className="font-semibold ml-1 mb-1 mt-4">Target Fund (ETH)</label>
-      <input type="text" className="input input-bordered input-sm w-1/2"/>
+        <label className="font-semibold ml-1 mb-1 mt-4">Target Fund (ETH)</label>
+        <input type="text" className="input input-bordered input-sm w-1/2"/>
 
-      <label className="font-semibold ml-1 mb-1 mt-4">Description</label>
-      <textarea className="textarea textarea-bordered h-64"/>
-    </div>
+        {/* https://stackoverflow.com/a/33822113 */}
+        <label htmlFor="upload-file" className="btn btn-neutral mt-4 w-1/3">{
+          uploading ?
+          <span className="loading loading-spinner"/> :
+          'Upload Image'
+        }</label>
+        <input id="upload-file"
+          type="file" className="hidden"
+          accept=".png,.jpeg,.jpg"
+          onChange={async (event) => {
+            if(event.target.files){
+              const file = event.target.files[0]
+              setUploading(true)
+              setImageUrl(await uploadImage(file))
+              setUploading(false)
+            }
+          }}
+        />
+        <Image src={imageUrl}
+          hidden={imageUrl===''} alt="image"
+          className="mt-4 rounded-box"
+          width={200} height={200}
+        />
+
+        <label className="font-semibold ml-1 mb-1 mt-4">Description</label>
+        <textarea className="textarea textarea-bordered h-64"/>
+      </div>
+    </>
   )
+}
+
+async function uploadImage(image: File): Promise<string> {
+  const formData = new FormData()
+  formData.append('image', image)
+  const res = await fetch('https://imgtp.com/api/upload', {
+    method: 'POST',
+    body: formData
+  })
+  const { data } = await res.json()
+  return data.url
 }
